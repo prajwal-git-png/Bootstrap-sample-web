33d19267657869fa86dc7aa9890d6fcc734e7370
Prajwal <86137538+prajwal-git-png@users.noreply.github.com>
2024-04-06 15:32:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true,
+
+// });
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
